1c6f80e99001e3bf1843ead27cf6740582499b26
Shwetabh <webwritat@gmail.com>
2025-04-03 02:45:10 +0530

    initial commit

--- site.py
+++ site.py
@@ -7,5 +7,5 @@ app.secret_key = 'rehgeriognerrghergn589y3y9uh&*ggn98HG(&g998RGHN*(fg98H84G'
 app.register_blueprint(main, url_prefix='/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
